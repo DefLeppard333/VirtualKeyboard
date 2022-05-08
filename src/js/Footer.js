82c45ef48b8d5cdace66dece462914b2a4eddebf
Igor Shpykuliak <defleppard333@gmail.com>
2022-05-09 00:20:26 +0300

    feat: keyboard fix

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Control {
     this.subtitlle = new Control(this.element, 'h2', 'subtitlle');
     this.subtitlle.element.textContent = 'Клавиатура создана в операционной системе Windows';
     this.subtitlle_second = new Control(this.element, 'h3', 'subtitlle');
-    this.subtitlle_second.element.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
+    this.subtitlle_second.element.textContent = 'Для переключения языка комбинация: левыe ctrl + shift';
   }
 }
 
